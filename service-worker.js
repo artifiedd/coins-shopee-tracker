2372500e1fc4cd70743b6adfe39c5721c6444035
artifiedd <nuramalya1o1@gmail.com>
2025-08-21 14:43:47 +0800

    Update the size for iphone

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shopee-tracker-v1";
+const CACHE_NAME = "shopee-tracker-v2";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -9,14 +9,27 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// Install SW
+// Install Service Worker
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// Serve files offline
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch cached files
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
